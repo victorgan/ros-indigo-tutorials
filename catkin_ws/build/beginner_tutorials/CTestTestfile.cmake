91ed0914f71fd24594fd661dcab0f7bab539b24a
Victor Gan <victorgan@gmail.com>
2015-03-11 14:10:34 -0700

    catkin_make in catkin_ws

---
+++ catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vgan/code/ros-indigo-tutorials/catkin_ws/src/beginner_tutorials
+# Build directory: /home/vgan/code/ros-indigo-tutorials/catkin_ws/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -125,6 +125,7 @@ ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unsp
 IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   INCLUDE("/home/vgan/code/ros-indigo-tutorials/catkin_ws/build/gtest/cmake_install.cmake")
+  INCLUDE("/home/vgan/code/ros-indigo-tutorials/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
 
 ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
 
