94a1b448977a7fd59593423f3051594dd9abe677
Victor Gan <victorgan@gmail.com>
2015-03-11 14:27:41 -0700

    catkin_make in catkin_ws, 3rd

--- catkin_ws/build/beginner_tutorials/catkin_generated/package.cmake
+++ catkin_ws/build/beginner_tutorials/catkin_generated/package.cmake
@@ -1,5 +1,5 @@
 set(_CATKIN_CURRENT_PACKAGE "beginner_tutorials")
-set(beginner_tutorials_MAINTAINER "vgan <vgan@todo.todo>")
+set(beginner_tutorials_MAINTAINER "Victor Gan <victorgan@gmail.com>")
 set(beginner_tutorials_DEPRECATED "")
 set(beginner_tutorials_VERSION "0.0.0")
 set(beginner_tutorials_BUILD_DEPENDS "roscpp" "rospy" "std_msgs")
